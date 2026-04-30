44c13c92dadf7dbbc2e0bce1ca4bf5df837ae29d
Chabib N. <chabib.nr@gmail.com>
2026-05-01 05:51:28 +0700

    setup

---
+++ tsdown.config.js
@@ -0,0 +1,21 @@
+import { fileURLToPath } from 'node:url'
+
+export { tsdownConfig as default }
+
+/** @type {import('tsdown').UserConfig} */
+const tsdownConfig = {
+  entry: {
+    index: 'src/index.ts',
+  },
+
+  alias: {
+    '#': fileURLToPath(new URL('src', import.meta.url)),
+  },
+
+  dts: {
+    tsconfig: 'tsconfig.build.json',
+  },
+
+  minify: true,
+  fixedExtension: false,
+}
