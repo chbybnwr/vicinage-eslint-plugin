58acaa7b3ef68a7e83645ad86af1df66417a0b5f
Chabib N. <chabib.nr@gmail.com>
2026-05-02 02:10:50 +0700

    refactor: index

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+export { rules }
+
+const rules = {
+  // 'no-conflicting-props': noConflictingProps,
+  // 'no-lookahead-selectors': noLookaheadSelectors,
+  // 'no-nonstandard-styles': noNonStandardStyles,
+  // 'sort-keys': sortKeys,
+  // 'valid-shorthands': validShorthands,
+  // 'valid-styles': validStyles,
+}
+
+// import noConflictingProps from './no-conflicting-props'
+// import noLookaheadSelectors from './no-lookahead-selectors'
+// import noNonStandardStyles from './no-nonstandard-styles'
+// import sortKeys from './sort-keys'
+// import validShorthands from './valid-shorthands'
+// import validStyles from './valid-styles'
