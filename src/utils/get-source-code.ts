32e462dcc424a0c684dacabd0ec5326f2fb7e008
Chabib N. <chabib.nr@gmail.com>
2026-05-04 03:52:15 +0700

    refactor: to typescript

--- src/utils/get-source-code.ts
+++ src/utils/get-source-code.ts
@@ -7,22 +7,23 @@
  * @flow strict
  */
 
-import type { SourceCode } from 'eslint/eslint-rule'
-/* :: import { Rule } from 'eslint'; */
+import type { Rule } from 'eslint'
+import type { SourceCode } from '@eslint/core'
 
 // Fallback to legacy `getSourceCode()` for compatibility with older ESLint versions
 export default function getSourceCode(context: Rule.RuleContext): SourceCode {
-  const sourceCode =
-    context.sourceCode ||
-    (typeof context.getSourceCode === 'function'
-      ? context.getSourceCode()
-      : null)
+  // const sourceCode =
+  //   context.sourceCode ||
+  //   (typeof context.getSourceCode === 'function'
+  //     ? context.getSourceCode()
+  //     : null)
+  const { sourceCode } = context
 
-  if (!sourceCode) {
-    throw new Error(
-      'ESLint context does not provide source code access. Please update ESLint to v>=8.40.0. See: https://eslint.org/blog/2023/09/preparing-custom-rules-eslint-v9/',
-    )
-  }
+  // if (!sourceCode) {
+  //   throw new Error(
+  //     'ESLint context does not provide source code access. Please update ESLint to v>=8.40.0. See: https://eslint.org/blog/2023/09/preparing-custom-rules-eslint-v9/',
+  //   )
+  // }
 
   return sourceCode
 }
