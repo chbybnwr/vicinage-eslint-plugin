cd8721265dfb393f22dd6c736d7e740e8dc99c21
Chabib N. <chabib.nr@gmail.com>
2026-05-05 05:18:14 +0700

    build: export rule sort-keys

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ const rules = {
   'no-conflicting-props': noConflictingProps,
   // 'no-lookahead-selectors': noLookaheadSelectors,
   // 'no-nonstandard-styles': noNonStandardStyles,
-  // 'sort-keys': sortKeys,
+  'sort-keys': sortKeys,
   'valid-shorthands': validShorthands,
   // 'valid-styles': validStyles,
 }
@@ -12,6 +12,6 @@ const rules = {
 import noConflictingProps from './no-conflicting-props'
 // import noLookaheadSelectors from './no-lookahead-selectors'
 // import noNonStandardStyles from './no-nonstandard-styles'
-// import sortKeys from './sort-keys'
+import sortKeys from './sort-keys'
 import validShorthands from './valid-shorthands'
 // import validStyles from './valid-styles'
