d0d1620be72328378fcc5dec8e5f5b39f9688466
Chabib N. <chabib.nr@gmail.com>
2026-05-22 10:59:20 +0700

    fix: enable 'valid-styles' rule in recommended config

--- src/index.ts
+++ src/index.ts
@@ -25,7 +25,7 @@ const plugin = {
       },
       rules: {
         'vicinage/no-conflicting-props': 'error',
-        'vicinage/valid-styles': 'off',
+        'vicinage/valid-styles': 'error',
         'vicinage/valid-shorthands': 'warn',
         'vicinage/sort-keys': 'warn',
       },
